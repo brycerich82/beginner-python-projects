c182e0c1346f86668e882e993c875439c5f84bf6
brycerich82 <bryce.rich82@gmail.com>
2026-07-15 14:24:02 -0400

    This is my 6th Project

---
+++ shoppingCartProgram.py
@@ -0,0 +1,27 @@
+# Shopping Cart Program
+
+items = []
+prices = []
+total = 0
+
+while True:
+    item = input("Enter an item to buy (q to quit): ")
+    if item.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of {item}: $"))
+        items.append(item)
+        prices.append(price)
+
+print()
+print("----- YOUR CART -----")
+print()
+
+for item in items:
+    print(item)
+
+for price in prices:
+    total += price
+
+print()
+print(f"Your total is: ${total:.2f}")
