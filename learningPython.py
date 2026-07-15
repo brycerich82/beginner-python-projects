7443a08c0a9ab8eacbc8c5d97c495518caedb483
brycerich82 <bryce.rich82@gmail.com>
2026-07-15 19:10:37 -0400

    7/15/2026

--- learningPython.py
+++ learningPython.py
@@ -887,44 +887,44 @@ capitals = {
 # print("Thanks for playing!")
 
 
-# Dice Roller Program
-import random
+# # Dice Roller Program
+# import random
 
-# unit code chars to make dice
+# # unit code chars to make dice
 
-# print("\u25cf \u250c \u2510 \u2510 \u2502 \u2514 \u2518")
+# # print("\u25cf \u250c \u2510 \u2510 \u2502 \u2514 \u2518")
 
-# should look like this:
+# # should look like this:
 
-# ● ┌ ┐ ┐ │ └ ┘
+# # ● ┌ ┐ ┐ │ └ ┘
 
-dice_art = {
-    1: ("┌─────────┐", "│         │", "│    ●    │", "│         │", "└─────────┘"),
-    2: ("┌─────────┐", "│  ●      │", "│         │", "│      ●  │", "└─────────┘"),
-    3: ("┌─────────┐", "│  ●      │", "│    ●    │", "│      ●  │", "└─────────┘"),
-    4: ("┌─────────┐", "│  ●   ●  │", "│         │", "│  ●   ●  │", "└─────────┘"),
-    5: ("┌─────────┐", "│  ●   ●  │", "│    ●    │", "│  ●   ●  │", "└─────────┘"),
-    6: ("┌─────────┐", "│  ●   ●  │", "│  ●   ●  │", "│  ●   ●  │", "└─────────┘"),
-}
+# dice_art = {
+#     1: ("┌─────────┐", "│         │", "│    ●    │", "│         │", "└─────────┘"),
+#     2: ("┌─────────┐", "│  ●      │", "│         │", "│      ●  │", "└─────────┘"),
+#     3: ("┌─────────┐", "│  ●      │", "│    ●    │", "│      ●  │", "└─────────┘"),
+#     4: ("┌─────────┐", "│  ●   ●  │", "│         │", "│  ●   ●  │", "└─────────┘"),
+#     5: ("┌─────────┐", "│  ●   ●  │", "│    ●    │", "│  ●   ●  │", "└─────────┘"),
+#     6: ("┌─────────┐", "│  ●   ●  │", "│  ●   ●  │", "│  ●   ●  │", "└─────────┘"),
+# }
 
-dice = []
-total = 0
-print()
-num_of_dice = int(input("Enter a number of die: "))
-
-for die in range(num_of_dice):
-    dice.append(random.randint(1, 6))
+# dice = []
+# total = 0
+# print()
+# num_of_dice = int(input("Enter a number of die: "))
 
 # for die in range(num_of_dice):
-#     for line in dice_art.get(dice[die]):   #This is for verticle die
-#         print(line)
+#     dice.append(random.randint(1, 6))
 
-for line in range(5):
-    for die in dice:
-        print(dice_art.get(die)[line], end="")  # This is for horizontal die
-    print()
+# # for die in range(num_of_dice):
+# #     for line in dice_art.get(dice[die]):   #This is for verticle die
+# #         print(line)
 
-for die in dice:
-    total += die
-print()
-print(f"The total is: {total}!")
+# for line in range(5):
+#     for die in dice:
+#         print(dice_art.get(die)[line], end="")  # This is for horizontal die
+#     print()
+
+# for die in dice:
+#     total += die
+# print()
+# print(f"The total is: {total}!")
